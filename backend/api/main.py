139cc6f48a2b6a6d814df8eb6e2fe52d85941134
HemanthVarmaKonduru <155761153+HemanthVarmaKonduru@users.noreply.github.com>
2025-11-07 23:16:29 -0600

    cors

--- backend/api/main.py
+++ backend/api/main.py
@@ -42,7 +42,7 @@ app = FastAPI(
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=CORS_ORIGINS,
+    allow_origins=["http://localhost:3000", "http://localhost:3001","https://ai-crash-test-prototype.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
